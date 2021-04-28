465f151cab934395212ac1a15a7f6331f97cebaa
Neal Robben <neal.robben@gmail.com>
2021-04-29 00:57:09 +0200

    Implemented geolocation

--- DriverApp/ColdStartChallenge.DriverApp/ColdStartChallenge.DriverApp/Services/LocationService.cs
+++ DriverApp/ColdStartChallenge.DriverApp/ColdStartChallenge.DriverApp/Services/LocationService.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
+using static Xamarin.Essentials.Permissions;
 
 namespace ColdStartChallenge.DriverApp.Services
 {
@@ -14,7 +15,19 @@ namespace ColdStartChallenge.DriverApp.Services
         {
             try
             {
-                // *** GET THE CURRENT LOCATION ***
+                var status = await GetStatus();
+                if (status == PermissionStatus.Granted)
+                {
+                    var request = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
+                    _cts = new CancellationTokenSource();
+                    var location = await Geolocation.GetLocationAsync(request, _cts.Token);
+
+                    if (location != null)
+                    {
+                        Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                        return location;
+                    }
+                }
             }
             catch (Exception ex)
             {
@@ -30,11 +43,31 @@ namespace ColdStartChallenge.DriverApp.Services
             return null;
         }
 
+        public async Task<PermissionStatus> GetStatus()
+        {
+            var status = await CheckStatusAsync<LocationWhenInUse>();
+
+            if (status == PermissionStatus.Granted)
+                return status;
+
+            if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                return status;
+            }
+
+            status = await RequestAsync<LocationWhenInUse>();
+
+            return status;
+        }
+
         public async Task<(Location currentLocation, Placemark place)?> GetLocationDetail()
         {
             try
             {
-                // *** GECODE THE CURRENT LOCATION **
+                var location = await GetLocation();
+                var place = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+                (Location currentLocation, Placemark place)? x = (location, place?.FirstOrDefault());
+                return x;
             }
             catch (Exception ex)
             {               
